9bf5885cb974de38a1e9bfb4b6d5e89b47efbacb
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-05-30 14:46:19 +0000

    Understood about Date method in Javascript, Fetched current date using toLocalCurrentDate().

---
+++ 01_Basics/04_dates.js
@@ -0,0 +1,8 @@
+let currentMonth = new Date()
+
+console.log(currentMonth.toLocaleString('default',{
+    month:"long"
+}));
+
+console.log(Math.random()*10)
+console.log(currentMonth.getFullYear());
