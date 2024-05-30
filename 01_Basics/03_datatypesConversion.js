5a9e20fa18e4fa82b801e70640dce478ff3fa18c
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-05-30 14:44:41 +0000

    Learned about Datat type conversion e.g. Number to String , Learned about Comparision operators, String concatination, Accessing varibales using $ symbol and Converting Number to the Indian standards using toLocaleString('en-IN') method.

---
+++ 01_Basics/03_datatypesConversion.js
@@ -0,0 +1,38 @@
+let Number = 35
+
+// console.log(typeof Number);
+
+let valueInString = String(Number)
+// console.log(typeof valueInString);
+// console.log(valueInString);
+
+let Value = 45
+let negativeValue = -Value
+
+// console.log(negativeValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1= "hello"
+let str2=" Mayur"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+let accountName = "Mayur"
+let pageName = "Banking Details Page!"
+
+// console.log(`Welcome ${accountName} to ${pageName}`);
+
+
+let subjectName = new String("Marathi")
+// console.log(subjectName);
+
+const newNumber = 100000
+console.log(newNumber.toLocaleString('en-IN'));
