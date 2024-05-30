550d39397a677d137684b711a1314d36249f38fc
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-05-30 14:40:12 +0000

    all variables accessed using table method

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 2334
+let accountName= "Mayur"
+var password = 23345
+
+// accountId = 455 
+accountName = "Mayur"
+let accountEmail = "mayur.dalimbkar@tcs.com"
+let accountCity = "Pune"
+let accountAge = 23
+let accountAddress;
+
+// console.log(accountName);
+
+console.table([[accountName,accountEmail,accountCity,accountAge,accountAddress]])
