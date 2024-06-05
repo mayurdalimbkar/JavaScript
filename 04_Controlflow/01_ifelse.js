8cd4bd9521e8abb46a88c2a86884cd73b7b53154
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-06-05 14:16:27 +0000

    Understood about If esle conditionals.

---
+++ 04_Controlflow/01_ifelse.js
@@ -0,0 +1,18 @@
+const number =35
+
+// if(number ===36){
+//     console.log("mumber is equal");
+// }
+// else{
+//     console.log("number is not equal");
+// }
+
+
+
+const age = 25
+
+if(age>18){
+    console.log("You're an Adult.");
+}else{
+    console.log("You are kid.");
+}
