6d497cb66f03e11afcc6c046397496484f862c2a
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-06-05 14:25:08 +0000

    Learned about control flow of if else.

--- 04_Controlflow/01_ifelse.js
+++ 04_Controlflow/01_ifelse.js
@@ -16,3 +16,21 @@ if(age>18){
 }else{
     console.log("You are kid.");
 }
+
+const balance = 1000
+
+if(balance === 1000)  console.log("Payment Successful.");   //Implicit scope
+
+
+
+const fruitsCount = 490
+
+if(fruitsCount <500){
+    console.log("less than 500");
+}
+else if(fruitsCount <750){
+    console.log("less than 750");
+}
+else{
+    console.log("Number is equal to 1000");
+}
