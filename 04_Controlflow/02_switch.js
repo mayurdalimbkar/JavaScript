a2de9a24bf76e7767068ca4a5ab118d404bccff7
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-06-06 13:54:29 +0000

    Learned about switch statement.

---
+++ 04_Controlflow/02_switch.js
@@ -0,0 +1,20 @@
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("february");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+
+    default:
+        console.log("default value marched.");
+        break;
+}
