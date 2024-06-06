748aca90ed7facc435a5bb4a593ea9cd250c542d
Mayur Dalimbkar <101816188+mayurdalimbkar@users.noreply.github.com>
2024-06-06 14:19:53 +0000

    studied Nullish coalescing operator as well as ternary operator.

---
+++ 04_Controlflow/03_truthy.js
@@ -0,0 +1,17 @@
+const userEmail ="mayur.d@google.com"
+
+if(userEmail){
+    console.log("Got user Email");
+}
+else{
+    console.log("Don't have user Email.");
+}
+
+
+//falsy values 
+
+// false, 0,-0, BigInt 0n, Nan,"",Null, Undefined
+
+//truthy values
+
+// "true", "false", "0", [], {}, function(){}
